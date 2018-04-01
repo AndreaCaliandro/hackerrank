f07abe9902cbeb728ad1779a850d3ce4683bdb13
Andrea Caliandro <andrea.caliandro@udemy.com>
2018-03-31 19:30:34 -0700

    main

--- CraigslistPostClassifier/main.py
+++ CraigslistPostClassifier/main.py
@@ -4,21 +4,21 @@
 
 import sys
 sys.path.insert(0, "/Users/andreacaliandro/PycharmProjects/hackerrank")
-for path in sys.path:
-    print path
 
 import pandas as pd
 import numpy as np
+from copy import copy, deepcopy
 from read_files import read_file, read_stdinput, json_like_input
 from bag_of_words import BagOfWords
 
 def train_by_section(train_df):
     ml_dic = {}
     for section in train_df['section'].unique():
+        print section
         df = train_df[train_df['section']==section]
         ml = BagOfWords(train_docs=df['heading'], train_target=df['category'])
         ml.modeling()
-        ml_dic[section] = ml
+        ml_dic[section] = deepcopy(ml)
     return ml_dic
 
 
@@ -34,7 +34,16 @@ for section in test_df['section'].unique():
         models_dic[section].predict(test_df[test_df['section'] == section]['heading'])
 
 test_target = read_file('./data/sample-test.out.json')
-score = (test_df['category'] == map(str.strip, test_target)).sum()/len(test_target)
+test_df['matching'] = (test_df['category'] == map(str.strip, test_target))
+score = test_df['matching'].sum()*1.0/len(test_target)
+print 'Total score =', score
+
+print 'Partial scores'
+for section in test_df['section'].unique():
+    score = test_df[test_df['section'] == section]['matching'].sum()*1.0/len(test_df[test_df['section'] == section])
+    print '{section} score = {score}'.format(section=section, score=score)
+
+
 
 
 
