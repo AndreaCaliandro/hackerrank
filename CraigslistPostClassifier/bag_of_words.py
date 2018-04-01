960f545210516bee6e3711ac2bdc8bcf3dfde6f7
Andrea Caliandro <andrea.caliandro@udemy.com>
2018-03-31 18:32:28 -0700

    main

--- CraigslistPostClassifier/bag_of_words.py
+++ CraigslistPostClassifier/bag_of_words.py
@@ -2,23 +2,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
 
+
 class BagOfWords():
 
-    def __init__(self, train_docs, train_target, test_docs, test_target,
+    def __init__(self, train_docs, train_target,
                  vectorizer=TfidfVectorizer(), classifier=MultinomialNB(alpha=.01)):
         self.train_docs = train_docs
         self.train_target = train_target
-        self.test_docs = test_docs
-        self.test_target = test_target
         self.vectorizer = vectorizer
         self.classifier = classifier
 
-    def tfidf(self):
-        self.train_tfidf = self.vectorizer.fit_transform(self.train_docs)
-        self.test_tfidf = self.vectorizer.transform(self.test_docs)
-
     def modeling(self):
+        self.train_tfidf = self.vectorizer.fit_transform(self.train_docs)
         self.classifier.fit(self.train_tfidf, self.train_target)
-        pred = self.classifier.predict(self.test_tfidf)
-        score = metrics.accuracy_score(y_test, pred)
+
+    def predict(self, test_docs):
+        self.test_tfidf = self.vectorizer.transform(test_docs)
+        return self.classifier.predict(self.test_tfidf)
+
+    def estimator(self, test_target, pred):
+        score = metrics.accuracy_score(test_target, pred)
         print("accuracy:   %0.3f" % score)
+
--- CraigslistPostClassifier/main.py
+++ CraigslistPostClassifier/main.py
@@ -7,8 +7,34 @@ sys.path.insert(0, "/Users/andreacaliandro/PycharmProjects/hackerrank")
 for path in sys.path:
     print path
 
-from read_files import read_file, json_like_input
+import pandas as pd
+import numpy as np
+from read_files import read_file, read_stdinput, json_like_input
+from bag_of_words import BagOfWords
+
+def train_by_section(train_df):
+    ml_dic = {}
+    for section in train_df['section'].unique():
+        df = train_df[train_df['section']==section]
+        ml = BagOfWords(train_docs=df['heading'], train_target=df['category'])
+        ml.modeling()
+        ml_dic[section] = ml
+    return ml_dic
+
 
 data = read_file('./data/training.json')
 train_df = json_like_input(data)
-train_df.head()
+models_dic = train_by_section(train_df)
+
+# test_data = read_stdinput()
+test_data = read_file('./data/sample-test.in.json')
+test_df = json_like_input(test_data)
+for section in test_df['section'].unique():
+    test_df.loc[test_df['section'] == section, 'category'] = \
+        models_dic[section].predict(test_df[test_df['section'] == section]['heading'])
+
+test_target = read_file('./data/sample-test.out.json')
+score = (test_df['category'] == map(str.strip, test_target)).sum()/len(test_target)
+
+
+
